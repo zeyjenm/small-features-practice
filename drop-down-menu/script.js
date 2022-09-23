759cd5640e63ddd62fc5a2e9fc1e3895eb0c203b
Zeynep <zeynepmarch@gmail.com>
2022-09-23 13:37:17 +1000

    Change mouseover eventlisteners to mouseenter and mouseleave

--- drop-down-menu/script.js
+++ drop-down-menu/script.js
@@ -7,40 +7,60 @@ clearAll();
 
 const dogs = document.querySelector('#dogs-h');
 
-dogs.addEventListener('mouseover', () => {
+dogs.addEventListener('mouseenter', () => {
     clearAll();
     const dropDown = document.querySelector('#dogs');
     dropDown.style.display = "contents";
 })
 
+dogs.addEventListener('mouseleave', () => {
+    clearAll();
+})
+
 const cats = document.querySelector('#cats-h');
 
-cats.addEventListener('mouseover', () => {
+cats.addEventListener('mouseenter', () => {
     clearAll();
     const dropDown = document.querySelector('#cats');
     dropDown.style.display = "contents";
 })
 
+cats.addEventListener('mouseleave', () => {
+    clearAll();
+})
+
 const birds = document.querySelector('#birds-h');
 
-birds.addEventListener('mouseover', () => {
+birds.addEventListener('mouseenter', () => {
     clearAll();
     const dropDown = document.querySelector('#birds');
     dropDown.style.display = "contents";
 })
 
+birds.addEventListener('mouseleave', () => {
+    clearAll();
+})
+
 const small = document.querySelector('#small-h');
 
-small.addEventListener('mouseover', () => {
+small.addEventListener('mouseenter', () => {
     clearAll();
     const dropDown = document.querySelector('#small');
     dropDown.style.display = "contents";
 })
 
+small.addEventListener('mouseleave', () => {
+    clearAll();
+})
+
 const aquatic = document.querySelector('#aquatic-h');
 
-aquatic.addEventListener('mouseover', () => {
+aquatic.addEventListener('mouseenter', () => {
     clearAll();
     const dropDown = document.querySelector('#aquatic');
     dropDown.style.display = "contents";
+})
+
+aquatic.addEventListener('mouseleave', () => {
+    clearAll();
 })
