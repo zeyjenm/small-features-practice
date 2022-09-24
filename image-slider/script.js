22f8afa347dce3cbbd15c629622ff228cc9ddec5
Zeynep <zeynepmarch@gmail.com>
2022-09-24 12:17:06 +1000

    Add circle buttons to scroll directly to specific images

--- image-slider/script.js
+++ image-slider/script.js
@@ -30,5 +30,25 @@ back.addEventListener('click', () => {
     return i;
 })
 
+const flamingoButton = document.querySelector('#one');
+const lizardButton = document.querySelector('#two');
+const birdButton = document.querySelector('#three');
 
+flamingoButton.addEventListener('click', () => {
+    scroll(imageArray[0]);
+    i = 0;
+    return i;
+})
+
+lizardButton.addEventListener('click', () => {
+    scroll(imageArray[1]);
+    i = 1;
+    return i;
+})
+
+birdButton.addEventListener('click', () => {
+    scroll(imageArray[2]);
+    i = 2;
+    return i;
+})
 
