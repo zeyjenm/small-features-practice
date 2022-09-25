6662b47e474fb780a1d1da8df1760c11e6e3173f
Zeynep <zeynepmarch@gmail.com>
2022-09-25 11:47:01 +1000

    Add custom validation messages for country and postcode

--- custom-form-validation/script.js
+++ custom-form-validation/script.js
@@ -1,7 +1,7 @@
 // document references for inputs
 
 const emailInput = document.querySelector('#email');
-const countryInput = document.querySelector('#text');
+const countryInput = document.querySelector('#country');
 const postcodeInput = document.querySelector('#postcode');
 const passwordInput = document.querySelector('#password');
 const confirmInput = document.querySelector('#confirm');
@@ -10,10 +10,38 @@ const confirmInput = document.querySelector('#confirm');
 
 emailInput.addEventListener('input', () => {
     if (emailInput.validity.typeMismatch) {
-        emailInput.setCustomValidity("Please enter a valid email");
+        emailInput.setCustomValidity("Please enter a valid email.");
         emailInput.reportValidity();
     } 
     else {
         emailInput.setCustomValidity("");
+    }
+});
+
+// validator for country
+
+countryInput.addEventListener('input', () => {
+    if (countryInput.validity.tooShort) {
+        countryInput.setCustomValidity("Please enter a country.");
+        countryInput.reportValidity();
+    }
+    else {
+        countryInput.setCustomValidity("");
+    }
+});
+
+postcodeInput.addEventListener('input', (e) => {
+    e.preventDefault();
+    if (postcodeInput.validity.rangeUnderflow) {
+        postcodeInput.setCustomValidity("Please enter a valid four digit postcode.");
+        postcodeInput.reportValidity();
+    }
+    else if (postcodeInput.validity.rangeOverflow) {
+        postcodeInput.setCustomValidity("Please enter a valid four digit postcode.");
+        postcodeInput.reportValidity();
+    }
+    else {
+        postcodeInput.setCustomValidity('');
+
     }
 });
